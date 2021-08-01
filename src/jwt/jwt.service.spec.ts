f24be79a0de2df7208428916d46635df77b45c7b
heecheol153 <heecheol153@gmail.com>
2021-08-01 16:27:18 +0900

    #8.1 JWT Sign Test

---
+++ src/jwt/jwt.service.spec.ts
@@ -0,0 +1,46 @@
+import { Test } from '@nestjs/testing';
+import * as jwt from 'jsonwebtoken';
+import { CONFIG_OPTIONS } from 'src/common/common.constants';
+import { JwtService } from './jwt.service';
+
+//import * as jwt from 'jsonwebtoken';에서 가져온다.
+jest.mock('jsonwebtoken', () => {
+  return {
+    sign: jest.fn(() => 'TOKEN'),
+  };
+});
+const TEST_KEY = 'testKey';
+
+describe('JwtService', () => {
+  let service: JwtService;
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        JwtService,
+        {
+          provide: CONFIG_OPTIONS,
+          useValue: { privateKey: TEST_KEY },
+        },
+      ],
+    }).compile();
+    service = module.get<JwtService>(JwtService);
+  });
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+  describe('sign', () => {
+    it('should return a signed token', () => {
+      const ID = 1;
+      const token = service.sign(ID);
+      console.log(token);
+      expect(typeof token).toBe('string');
+      expect(jwt.sign).toHaveBeenCalledTimes(1);
+      expect(jwt.sign).toHaveBeenLastCalledWith({ id: ID }, TEST_KEY);
+    });
+  });
+  describe('verify', () => {
+    it('should return the decoded token', () => {});
+  });
+  //it.todo('sign');
+  //it.todo('verify');
+});
