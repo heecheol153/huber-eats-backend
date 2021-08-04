2707755af194d68b7659a9d0c7c3fca4ee920bc8
heecheol153 <heecheol153@gmail.com>
2021-08-04 15:46:11 +0900

    #9.3 Testing createAccount part Two

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -51,8 +51,36 @@ describe('UserModule (e2e)', () => {
           expect(res.body.data.createAccount.error).toBe(null);
         });
     });
-
-    it.todo('should fail if account already exists');
+    //3.위의1번부터 복붙한다.
+    it('should fail if account already exists', () => {
+      return request(app.getHttpServer()) //1.request를 app.getHttpServer()로보냄
+        .post(GRAPHQL_ENDPOINT) //2.그리고 post를 GRAPHQL_ENDPOIT로 보낸다.위에const의 URL로 posting
+        .send({
+          // playground 부터 가져옴.
+          query: `
+        mutation {
+          createAccount(input: {
+            email:"${EMAIL}",
+            password:"12345",
+            role:Owner
+          }) {
+            ok
+            error
+          }
+        }
+        `,
+        })
+        .expect(200)
+        .expect((res) => {
+          // response도expect한다.
+          expect(res.body.data.createAccount.ok).toBe(false);
+          expect(res.body.data.createAccount.error).toBe(
+            'There is a user with that email already',
+          );
+          //expect(res.body.data.createAccount.error).toEqual(expect.any(String)); //toEqual은 어떤string이든 expect할수있게함.
+          //toBe 로도 쓸수도있다.
+        });
+    });
   });
 
   //아래순서대로 테스트를 진행한다.
