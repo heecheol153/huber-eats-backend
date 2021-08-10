9b281ea73fbbf4a778b026b2d7d601bc8dd5bb5c
heecheol153 <heecheol153@gmail.com>
2021-08-10 17:47:03 +0900

    #11.4 Order Entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,6 +19,8 @@ import { Restaurant } from './restaurants/entities/restaurant.entity';
 import { Category } from './restaurants/entities/category.entity';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 import { Dish } from './restaurants/entities/dish.entity';
+import { OrdersModule } from './orders/orders.module';
+import { Order } from './orders/entities/order.entity';
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -48,7 +50,7 @@ import { Dish } from './restaurants/entities/dish.entity';
       synchronize: process.env.NODE_ENV !== 'prod',
       logging:
         process.env.NODE_ENV !== 'prod' && process.env.NODE_ENV !== 'test',
-      entities: [User, Verification, Restaurant, Category, Dish],
+      entities: [User, Verification, Restaurant, Category, Dish, Order],
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
@@ -65,6 +67,7 @@ import { Dish } from './restaurants/entities/dish.entity';
     AuthModule,
     UsersModule,
     RestaurantsModule,
+    OrdersModule,
   ],
   controllers: [],
   providers: [],
---
+++ src/orders/entities/order.entity.ts
@@ -0,0 +1,60 @@
+import {
+  Field,
+  Float,
+  InputType,
+  ObjectType,
+  registerEnumType,
+} from '@nestjs/graphql';
+import { CoreEntity } from 'src/common/entities/core.entity';
+import { Dish } from 'src/restaurants/entities/dish.entity';
+import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
+import { User } from 'src/users/entities/user.entity';
+import { Column, Entity, JoinTable, ManyToMany, ManyToOne } from 'typeorm';
+
+export enum OrderStatus {
+  Pending = 'Pending', //대기
+  Cooking = 'Cooking', //조리
+  PickedUp = 'PickedUp', //픽업
+  Delivered = 'Delivered', //배달완료
+}
+
+registerEnumType(OrderStatus, { name: 'OrderStatus' });
+
+@InputType('OrderInputType', { isAbstract: true })
+@ObjectType()
+@Entity()
+export class Order extends CoreEntity {
+  @Field((type) => User, { nullable: true })
+  @ManyToOne((type) => User, (user) => user.orders, {
+    onDelete: 'SET NULL',
+    nullable: true,
+  })
+  customer?: User; //type는 User
+
+  @Field((type) => User, { nullable: true })
+  @ManyToOne((type) => User, (user) => user.rides, {
+    onDelete: 'SET NULL',
+    nullable: true,
+  })
+  driver?: User; //driver가배달해준다 이것도 User type,주문했을땐배달자가존재안함?을붙음.
+
+  @Field((type) => Restaurant)
+  @ManyToOne((type) => Restaurant, (restaurant) => restaurant.orders, {
+    onDelete: 'SET NULL',
+    nullable: true,
+  })
+  restaurant: Restaurant; //relationship과 상관없이 restaurant을 가짐
+
+  @Field((type) => [Dish])
+  @ManyToMany((type) => Dish)
+  @JoinTable()
+  dishes: Dish[]; //order는 dish목록가지고있다.배열이다.
+
+  @Column()
+  @Field((type) => Float) //9.99같은 가격도있으니까
+  total: number;
+
+  @Column({ type: 'enum', enum: OrderStatus })
+  @Field((type) => OrderStatus)
+  status: OrderStatus;
+}
