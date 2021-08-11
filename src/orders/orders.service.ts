81e50ff5e11c9927a98ae149f40125bdba6aa282
heecheol153 <heecheol153@gmail.com>
2021-08-11 15:54:19 +0900

    #11.10 Create Order part Five

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -32,26 +32,52 @@ export class OrderService {
         error: 'Resataurant not found',
       };
     }
-    items.forEach(async (item) => {
-      //console.log(item.options);
+    for (const item of items) {
+      //이item은 유저가 보낸것이다.
       const dish = await this.dishes.findOne(item.dishId);
       if (!dish) {
-        // abort this whole thing
+        return {
+          ok: false,
+          error: 'Dish not found.',
+        };
       }
-      await this.orderItems.save(
-        //dish찾으면orderItemd에dish,items을 넣는다.
+      console.log(`Dish price: ${dish.price}`); //전체주문이 얼마인지알수있다.
+      for (const itemOption of item.options) {
+        const dishOption = dish.options.find(
+          (dishOption) => dishOption.name === itemOption.name,
+        );
+        //console.log(itemOption.name, dishOption.name);
+        if (dishOption) {
+          if (dishOption.extra) {
+            console.log(`$USD + ${dishOption.extra}`);
+          } else {
+            const dishOptionChoice = dishOption.choices.find(
+              (optionChoice) => optionChoice.name === itemOption.choice,
+            );
+            //console.log(dishOptionChoice);
+            if (dishOptionChoice) {
+              if (dishOptionChoice.extra) {
+                console.log(`$USD + ${dishOptionChoice.extra}`);
+              }
+            }
+          }
+        }
+      }
+      /*await this.orderItems.save(    유저가 지불해야할금액을 계산위해 Comment처리함
         this.orderItems.create({
           dish,
-          options: item.options,
+          options: item.optins,
         }),
       );
     });
+      ); */
+    }
     /* const order = await this.orders.save(
       this.orders.create({
         customer,
         restaurant,
       }),
     );
-    console.log(order);*/
+    console.log(order); */
   }
 }
