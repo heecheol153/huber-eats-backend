05da60a85b0a6d8cf3d8bbcd2a36d00d3165767a
heecheol153 <heecheol153@gmail.com>
2021-08-12 20:55:30 +0900

    #12.4 PUB_SUB

--- src/orders/orders.resolver.ts
+++ src/orders/orders.resolver.ts
@@ -1,7 +1,9 @@
+import { Inject } from '@nestjs/common';
 import { Args, Mutation, Resolver, Query, Subscription } from '@nestjs/graphql';
 import { PubSub } from 'graphql-subscriptions';
 import { AuthUser } from 'src/auth/auth-user.decorator';
 import { Role } from 'src/auth/role.decorator';
+import { PUB_SUB } from 'src/common/common.constants';
 import { User } from 'src/users/entities/user.entity';
 import { CreateOrderInput, CreateOrderOutput } from './dtos/create-order.dto';
 import { EditOrderInput, EditOrderOutput } from './dtos/edit-order.dto';
@@ -10,10 +12,13 @@ import { GetOrdersInput, GetOrdersOutput } from './dtos/get-orders.dto';
 import { Order } from './entities/order.entity';
 import { OrderService } from './orders.service';
 
-const pubsub = new PubSub();
+//const pubsub = new PubSub();//전체application에서하나여야한다.
 @Resolver((of) => Order)
 export class OrderResolver {
-  constructor(private readonly ordersService: OrderService) {}
+  constructor(
+    private readonly ordersService: OrderService,
+    @Inject(PUB_SUB) private readonly pubSub: PubSub,
+  ) {}
 
   @Mutation((returns) => CreateOrderOutput)
   @Role(['Client'])
@@ -54,7 +59,7 @@ export class OrderResolver {
 
   @Mutation((returns) => Boolean)
   potatoReady() {
-    pubsub.publish('hotPotatos', {
+    this.pubSub.publish('hotPotatos', {
       readyPotato: 'Your potato is ready. love you.',
     });
     return true;
@@ -65,6 +70,6 @@ export class OrderResolver {
   @Role(['Any'])
   readyPotato(@AuthUser() user: User) {
     console.log(user);
-    return pubsub.asyncIterator('hotPotatos');
+    return this.pubSub.asyncIterator('hotPotatos');
   }
 }
