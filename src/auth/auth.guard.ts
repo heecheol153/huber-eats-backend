0c3336389cf7654b5bf681bea7e1bafc3b7a05c4
heecheol153 <heecheol153@gmail.com>
2021-08-12 20:22:05 +0900

    #12.3 Subscription Authentication part Two

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,33 +1,46 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { GqlExecutionContext } from '@nestjs/graphql';
-import { User } from 'src/users/entities/user.entity';
+import { Token } from 'graphql';
+import { JwtService } from 'src/jwt/jwt.service';
+import { UserService } from 'src/users/users.service';
 import { AllowedRoles } from './role.decorator';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   //metadata를 얻기위해서 reflector를 얻는다.
-  constructor(private readonly reflector: Reflector) {}
-  canActivate(context: ExecutionContext) {
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly jwtService: JwtService,
+    private readonly userService: UserService,
+  ) {}
+  async canActivate(context: ExecutionContext) {
     const roles = this.reflector.get<AllowedRoles>(
       'roles',
       context.getHandler(),
     );
     //console.log('roles');
     if (!roles) {
+      //roles가 없으면 guard가 그냥지나간다.roles가없으면 보호할필요가 없기때문이다.
       return true;
     }
     //console.log(context);
     const gqlContext = GqlExecutionContext.create(context).getContext();
-    console.log(gqlContext.token);
-    const user: User = gqlContext['user'];
-    //console.log(user);
-    if (!user) {
-      return false; //user찾지못했다.login하지않았기때문에 user를 만들수없다는의미.
+    const token = gqlContext.token;
+    if (token) {
+      //context에토큰이 있으면 토큰을 decode한다.그리고 user를 찾는다. user가없으면 false를 return한다.
+      const decoded = this.jwtService.verify(token.toString());
+      if (typeof decoded === 'object' && decoded.hasOwnProperty('id')) {
+        const { user } = await this.userService.findById(decoded['id']);
+        if (user) {
+          gqlContext['user'] = user;
+          if (roles.includes('Any')) {
+            return true;
+          }
+          return roles.includes(user.role);
+        }
+      }
     }
-    if (roles.includes('Any')) {
-      return true;
-    }
-    return roles.includes(user.role);
+    return false;
   }
 }
