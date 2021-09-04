8e2824e83bfb12de66f708d2253459d6cce13565
heecheol153 <heecheol153@gmail.com>
2021-09-04 21:05:41 +0900

    #20.2 File Upload part One

--- src/restaurants/dtos/my-restaurants.dto.ts
+++ src/restaurants/dtos/my-restaurants.dto.ts
@@ -2,7 +2,7 @@ import { Field, ObjectType } from '@nestjs/graphql';
 import { CoreOutput } from 'src/common/dtos/output.dto';
 import { Restaurant } from '../entities/restaurant.entity';
 
-ObjectType();
+@ObjectType()
 export class MyRestaurantsOutput extends CoreOutput {
   @Field((type) => [Restaurant])
   restaurants?: Restaurant[];
